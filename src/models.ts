e03160d04d86444b0150909c0fb0bbd2a151219f
heimoshuiyu <heimoshuiyu@gmail.com>
2023-06-14 12:10:18 +0800

    openai model update

--- src/models.ts
+++ src/models.ts
@@ -9,21 +9,41 @@ interface Model {
 const models: Record<string, Model> = {
   "gpt-3.5-turbo": {
     maxToken: 4096,
-    price: { prompt: 0.002 / 1000, completion: 0.002 / 1000 },
+    price: { prompt: 0.0015 / 1000, completion: 0.002 / 1000 },
+  },
+  "gpt-3.5-turbo-16k": {
+    maxToken: 16384,
+    price: { prompt: 0.0003 / 1000, completion: 0.004 / 1000 },
+  },
+  "gpt-3.5-turbo-0613": {
+    maxToken: 4096,
+    price: { prompt: 0.0015 / 1000, completion: 0.002 / 1000 },
+  },
+  "gpt-3.5-turbo-16k-0613": {
+    maxToken: 16384,
+    price: { prompt: 0.0003 / 1000, completion: 0.004 / 1000 },
   },
   "gpt-3.5-turbo-0301": {
     maxToken: 4096,
-    price: { prompt: 0.002 / 1000, completion: 0.002 / 1000 },
+    price: { prompt: 0.0015 / 1000, completion: 0.002 / 1000 },
   },
   "gpt-4": {
     maxToken: 8192,
     price: { prompt: 0.03 / 1000, completion: 0.06 / 1000 },
   },
-  "gpt-4-0314": {
+  "gpt-4-0613": {
     maxToken: 8192,
     price: { prompt: 0.03 / 1000, completion: 0.06 / 1000 },
   },
   "gpt-4-32k": {
+    maxToken: 8192,
+    price: { prompt: 0.06 / 1000, completion: 0.12 / 1000 },
+  },
+  "gpt-4-32k-0613": {
+    maxToken: 8192,
+    price: { prompt: 0.06 / 1000, completion: 0.12 / 1000 },
+  },
+  "gpt-4-0314": {
     maxToken: 8192,
     price: { prompt: 0.03 / 1000, completion: 0.06 / 1000 },
   },
