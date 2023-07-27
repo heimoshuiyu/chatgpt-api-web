c90e32d74f929cfbd72757f0bb4c7fa8e1214e1d
heimoshuiyu <heimoshuiyu@gmail.com>
2023-07-27 11:32:36 +0800

    change default temperature from 1.0 to 0.7
    When developing prompt, it is generally better use a lower temperature
    from beginning

--- src/chatgpt.ts
+++ src/chatgpt.ts
@@ -59,7 +59,7 @@ class Chat {
       tokens_margin = 1024,
       apiEndPoint = "https://api.openai.com/v1/chat/completions",
       model = "gpt-3.5-turbo",
-      temperature = 1.0,
+      temperature = 0.7,
       top_p = 1,
       presence_penalty = 0,
       frequency_penalty = 0,
