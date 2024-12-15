3cd6bc5bc20bb55722656545ec881a44b4d705c6
heimoshuiyu <heimoshuiyu@gmail.com>
2024-12-15 22:16:49 +0800

    Handle NaN values in total cost calculations

--- src/utils/totalCost.ts
+++ src/utils/totalCost.ts
@@ -1,6 +1,9 @@
 import { STORAGE_NAME_TOTALCOST } from "@/const";
 
 export function addTotalCost(cost: number) {
+  if (isNaN(cost)) {
+    return;
+  }
   let totalCost = getTotalCost();
   totalCost += cost;
   localStorage.setItem(STORAGE_NAME_TOTALCOST, `${totalCost}`);
@@ -10,6 +13,9 @@ export function getTotalCost(): number {
   let totalCost = parseFloat(
     localStorage.getItem(STORAGE_NAME_TOTALCOST) ?? "0",
   );
+  if (isNaN(totalCost)) {
+    totalCost = 0;
+  }
   return totalCost;
 }
 
