a56aa0205db2405bf4c0c1e4dd1b94c3a89270ed
heimoshuiyu <heimoshuiyu@gmail.com>
2024-12-30 15:51:38 +0800

    menu flex wrap

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -30,7 +30,7 @@ const NavigationMenuList = React.forwardRef<
   <NavigationMenuPrimitive.List
     ref={ref}
     className={cn(
-      "group flex flex-1 list-none items-center justify-center space-x-1",
+      "group flex flex-1 flex-wrap list-none items-center justify-center space-x-1",
       className
     )}
     {...props}
